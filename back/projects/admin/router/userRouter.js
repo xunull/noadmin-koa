9ef910153e6e53666024577d89d97ff5ed6e5030
xunull <todogo@vip.163.com>
2016-12-19 15:33:42 +0800

    koa-router的path 问题

--- back/projects/admin/router/userRouter.js
+++ back/projects/admin/router/userRouter.js
@@ -1,14 +1,20 @@
-var Router = require('koa-router');
-var userController = require('../controllers/userController');
-var basicController = require('../controllers/basicController');
+var Router = require('koa-router')
+var userController = require('../controllers/userController')
 
+/**
+ * koa-router 一个大问题
+ * router的path 如果以/开头, 那么这个path 就是一个相对于网站url root的一个path,
+ * 即使对于嵌套路由来说,因此如果是嵌套路由,那么prefix 不能以/开头
+ * 
+ * 如果一个路由使用了前缀,那么 此路由中的path 需要以/开头
+ */
 
-// var router = new Router({
-// 	prefix: '/user'
-// });
-var router = new Router();
-// router.prefix('/user');
-router.get('user/indexMenu',userController.indexMenu);
-// router.get('indexMenu',userController.indexMenu);
+var router = new Router({
+	prefix: 'user'
+})
 
-module.exports=router;
+router.get('/indexMenu',userController.indexMenu)
+
+router.get('/getRouter',userController.getRouter)
+
+module.exports=router
