771f935b0df672e9effc317a79bcbdc6d90b0fb9
xunull <todogo@vip.163.com>
2016-12-21 10:43:54 +0800

    输出banner 使用rainbow ,#给生活添点颜色

--- back/projects/admin/index.js
+++ back/projects/admin/index.js
@@ -1,12 +1,16 @@
-var path = require('path');
-var co = require('co');
+const path = require('path')
+const co = require('co')
+const colors = require('colors/safe')
 
-var mongoose= require('../../dawan/common/db/mongoose');
-var config=global.dawan.config;
-var logger = global.dawan.logger;
+var mongoose= require('../../dawan/common/db/mongoose')
 
-var common = global.dawan.common;
+var config=global.dawan.config
+var logger = global.dawan.logger
 
+var common = global.dawan.common
+var mysql = common.db.mysql
+
+global.projects.admin={}
 
 // async function  readBanner(){
 // 	var banner = await common.file.readFile(path.resolve(__dirname,'./banner.txt'))
@@ -17,7 +21,8 @@ var common = global.dawan.common;
 
 co(function * readBanner() {
 	var banner = yield common.file.readFile(path.resolve(__dirname,'./banner.txt'))
-	console.log(banner.toString())
+	// console.log(banner.toString())
+	console.log(colors.rainbow(banner.toString()))
 })
 
 // var fn = co.wrap(function* (val) {
@@ -28,4 +33,11 @@ co(function * readBanner() {
 //
 // });
 
-mongoose.connect(config.db.mongodb);
+mongoose.connect(config.db.mongodb)
+let mysqlConfig = config.db.mysql
+var connection = mysql.connect(mysqlConfig.host,mysqlConfig.user,mysqlConfig.password,mysqlConfig.database)
+
+
+global.projects.admin.connection = connection
+
+global.projects.admin.mysqlDao = require('./mysql/dao')(connection)
